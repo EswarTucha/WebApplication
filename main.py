14f1cd9e22ebba4d155e16e8d3de37a51b64499c
EswarTucha <eswar.tucha@nagarro.com>
2025-02-14 11:00:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from app import app
 import webbrowser
 if __name__ == "__main__":
-    webbrowser.open_new('http://localhost:5000/')
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    webbrowser.open_new('http://localhost:10000/')
+    app.run(host="0.0.0.0", port=10000, debug=True)
 
